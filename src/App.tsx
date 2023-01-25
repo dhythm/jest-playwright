9dead17c152d989edf627f039f919b7bf803ff7d
dhythm <yuta.okada.20@gmail.com>
2023-01-25 23:40:42 +0900

    eliminate a couple of eslint error

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,10 @@ function App() {
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <div className="App">
         <div>
-          <a href="https://vitejs.dev" target="_blank">
+          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
             <img src="/vite.svg" className="logo" alt="Vite logo" />
           </a>
-          <a href="https://reactjs.org" target="_blank">
+          <a href="https://reactjs.org" target="_blank" rel="noreferrer"> 
             <img src={reactLogo} className="logo react" alt="React logo" />
           </a>
         </div>
