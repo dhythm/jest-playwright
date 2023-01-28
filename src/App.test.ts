9ab3026fe0dc240a2e6e20490b055e69cd43e078
dhythm <yuta.okada.20@gmail.com>
2023-01-28 22:29:27 +0900

    collect the coverage of playwright E2E test

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,10 +1,10 @@
 import { test, expect } from './baseFixtures';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto("http://localhost:5173/")
-})
-
-test('should be renderred', async ({ page }) => {
-  await expect(page.getByText('Vite + React')).toBeVisible()
+  await page.goto('http://localhost:5173/');
 });
 
+test('should be renderred', async ({ page }) => {
+  await expect(page.getByText('Vite + React')).toBeVisible();
+  await page.getByRole('button').click()
+});
--- src/baseFixtures.ts
+++ src/baseFixtures.ts
@@ -13,19 +13,34 @@ export const test = baseTest.extend({
   context: async ({ context }, use) => {
     await context.addInitScript(() =>
       window.addEventListener('beforeunload', () =>
-        (window as any).collectIstanbulCoverage(JSON.stringify((window as any).__coverage__))
-      ),
+        (window as any).collectIstanbulCoverage(
+          JSON.stringify((window as any).__coverage__)
+        )
+      )
     );
     await fs.promises.mkdir(istanbulCLIOutput, { recursive: true });
-    await context.exposeFunction('collectIstanbulCoverage', (coverageJSON: string) => {
-      if (coverageJSON)
-        fs.writeFileSync(path.join(istanbulCLIOutput, `playwright_coverage_${generateUUID()}.json`), coverageJSON);
-    });
+    await context.exposeFunction(
+      'collectIstanbulCoverage',
+      (coverageJSON: string) => {
+        if (coverageJSON)
+          fs.writeFileSync(
+            path.join(
+              istanbulCLIOutput,
+              `playwright_coverage_${generateUUID()}.json`
+            ),
+            coverageJSON
+          );
+      }
+    );
     await use(context);
     for (const page of context.pages()) {
-      await page.evaluate(() => (window as any).collectIstanbulCoverage(JSON.stringify((window as any).__coverage__)))
+      await page.evaluate(() =>
+        (window as any).collectIstanbulCoverage(
+          JSON.stringify((window as any).__coverage__)
+        )
+      );
     }
-  }
+  },
 });
 
 export const expect = test.expect;
